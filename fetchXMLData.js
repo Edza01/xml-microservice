eff9e7ede7e6f3e329e16073b4176ffccc8d8505
Edza <miclex82@tutanota.com>
2023-09-28 12:29:43 +0300

    db

--- fetchXMLData.js
+++ fetchXMLData.js
@@ -1,12 +1,27 @@
 const axios = require('axios');
 const xml2js = require('xml2js');
 const fs = require('fs');
+const { Pool } = require('pg');
 
-// Define the URL of your supplier's XML data endpoint
-const supplierXMLUrl = '';
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'xmldatadb', 
+  password: 'root',
+  port: 5432, // PostgreSQL default port
+});
 
 async function fetchAndProcessSupplierData() {
-    try {
+  try {
+    // Fetch URLs from the PostgreSQL database
+    const dbQuery = 'SELECT link FROM xmldatatable'; // Replace with your table name
+    const dbResponse = await pool.query(dbQuery);
+    
+    if (dbResponse.rows.length > 0) {
+      // Iterate through the rows and fetch/process data for each URL
+      for (const row of dbResponse.rows) {
+        const supplierXMLUrl = row.link;
+        
         // Fetch XML data from the supplier
         const response = await axios.get(supplierXMLUrl);
     
@@ -15,17 +30,21 @@ async function fetchAndProcessSupplierData() {
           const xmlParser = new xml2js.Parser();
           const parsedData = await xmlParser.parseStringPromise(response.data);
           
-          // Store the parsed data in a JSON file
+          // Store the parsed data in a JSON file (you may want to store it differently)
           const jsonData = JSON.stringify(parsedData);
           fs.writeFileSync('supplierData.json', jsonData);
     
-          console.log('XML data fetched, parsed, and stored successfully.');
+          console.log('XML data fetched, parsed, and stored successfully for URL:', supplierXMLUrl);
         } else {
-          console.error('Failed to fetch XML data. Status code:', response.status);
+          console.error('Failed to fetch XML data for URL:', supplierXMLUrl, 'Status code:', response.status);
         }
-    } catch (error) {
-        console.error('Error fetching, parsing, and storing XML data:', error.message);
+      }
+    } else {
+      console.error('No URLs found in the database.');
     }
+  } catch (error) {
+    console.error('Error fetching, parsing, and storing XML data:', error.message);
+  }
 }
 
 module.exports = fetchAndProcessSupplierData;
